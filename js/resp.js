cb2e0a742fe75f177a91624123d40df6a4389519
Tulsi Dey <70888802+deytulsi18@users.noreply.github.com>
2021-04-17 16:36:41 +0530

    initial commit

---
+++ js/resp.js
@@ -0,0 +1,7 @@
+
+//  toggle menu button for responsiveness
+function toggleMenu() {
+    const menuIcon = document.querySelector('.menuIcon');
+    menuIcon.classList.toggle('active');
+    navBar.classList.toggle('active');
+}
